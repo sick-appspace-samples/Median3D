46f1544938e7bb65d65970d7295e6b5024e6f9eb
Florian Roser <Florian.Roser@sick.de>
2019-08-26 18:42:50 +0200

    'Update'

--- Median3D/scripts/Median3D.lua
+++ Median3D/scripts/Median3D.lua
@@ -16,18 +16,19 @@
 
   More Information:
   Tutorial "Algorithms - Filtering and Arithmetic".
-
 ------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 
-Script.serveEvent('Median3D.OnMessage1', 'OnMessage1', 'string')
-Script.serveEvent('Median3D.OnMessage2', 'OnMessage2', 'string')
+Script.serveEvent('Median3D.OnMessage1', 'OnMessage1')
+Script.serveEvent('Median3D.OnMessage2', 'OnMessage2')
 
 -- Create viewer for original and filtered 3D image
 local viewer1 = View.create()
 viewer1:setID('viewer1')
 local viewer2 = View.create()
 viewer2:setID('viewer2')
+
+local DELAY = 2000
 
 --End of Global Scope-----------------------------------------------------------
 
@@ -37,9 +38,13 @@ viewer2:setID('viewer2')
 local function filteringImage(heightMap, intensityMap)
   -- MEDIAN: Applies median filter to the image to smooth and remove noise
 
+  local hmDeco = View.ImageDecoration.create()
+  local minR, maxR = heightMap:getMinMax()
+  hmDeco:setRange(minR, maxR)
+
   -- Visualize the input (original image)
   viewer1:clear()
-  viewer1:addHeightmap({heightMap, intensityMap}, {}, {'Reflectance'})
+  viewer1:addHeightmap({heightMap, intensityMap}, {hmDeco}, {'Reflectance'})
   viewer1:present()
 
   Script.notifyEvent('OnMessage1', 'Original image')
@@ -51,24 +56,26 @@ local function filteringImage(heightMap, intensityMap)
 
   -- Visualize the output (median image)
   viewer2:clear()
-  viewer2:addHeightmap({medianImage, medianIntensityMap}, {}, {'Reflectance'})
+  viewer2:addHeightmap({medianImage, medianIntensityMap}, {hmDeco}, {'Reflectance'})
   viewer2:present()
 
   Script.notifyEvent('OnMessage2', 'Median filter, kernel size: ' .. kernelsize)
 end
 
 local function main()
-  -- Load a json-image
-  local data = Object.load('resources/image_23.json')
+  for i = 1, 2 do
+    -- Load a json-image
+    local data = Object.load('resources/image_' .. i .. '.json')
 
-  -- Extract heightmap, intensity map and sensor data
-  local heightMap = data[1]
-  local intensityMap = data[2]
-  local sensorData = data[3] --luacheck: ignore
+    -- Extract heightmap, intensity map and sensor data
+    local heightMap = data[1]
+    local intensityMap = data[2]
+    local sensorData = data[3] --luacheck: ignore
 
-  -- Filter image
-  filteringImage(heightMap, intensityMap)
-
+    -- Filter image
+    filteringImage(heightMap, intensityMap)
+    Script.sleep(DELAY)
+  end
   print('App finished')
 end
 --The following registration is part of the global scope which runs once after startup
