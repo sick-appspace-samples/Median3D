967a1ff3aed171a0d01fe62148c0f89aaff11ec0
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-09-08 12:51:12 +0200

    'Update'

--- Median3D/scripts/Median3D.lua
+++ Median3D/scripts/Median3D.lua
@@ -1,22 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  Median3D
-
-  Description:
-  Applying Median filter on a 3D image.
-
-  How to Run:
-  Starting this sample is possible either by running the app (F5) or
-  debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
-  function allows debugging step-by-step after 'Engine.OnStarted' event.
-  Results can be seen in the viewer on the DevicePage.
-  Select Reflectance in the View: box at the top of the GUI and zoom in on the
-  data for best experience.
-
-  More Information:
-  Tutorial "Algorithms - Filtering and Arithmetic".
-------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 
 Script.serveEvent('Median3D.OnMessage1', 'OnMessage1')
